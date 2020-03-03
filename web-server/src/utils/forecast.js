8809cdea16c060427eb750ea5b9937bf55e4345b
aimeelynnramirez <aimeelynnramirez@gmail.com>
2020-03-03 12:15:21 -0500

    commiting the change on neg

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            const convertTemp =  body.currently.temperature *= 1;
+            const convertTemp =  body.currently.temperature *= -1;
             const convertF = (convertTemp * 9/5) + 32;
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + convertF.toFixed(2) + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
    // console.log(body.currently.temperature * -1);     
